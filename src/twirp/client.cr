5995fa806acb764e352333b4cf9edce8b42cedc5
Jamie Gaskins <jgaskins@hey.com>
2022-12-06 21:35:10 -0500

    Use a connection pool
    This commit uses the DB::Pool class for a thread-safe, configurable
    connection pool. By default, it spins up a single connection on
    instantiation and will close all but one connection on checking
    connections back into the pool. This commit does not add configurability
    to that connection pool, but it can be added in a followup commit.

--- src/twirp/client.cr
+++ src/twirp/client.cr
@@ -1,46 +1,43 @@
 require "http/client"
+require "db/pool"
 
 require "./error"
 
 module Twirp
   class Client(T)
-    @callback : (HTTP::Request ->)?
+    @callback : HTTP::Request -> = Proc(HTTP::Request, Nil).new { }
 
     def initialize(@uri : URI)
       @prefix = uri.path.presence || "/twirp"
+      @pool = DB::Pool(HTTP::Client).new do
+        http = HTTP::Client.new(@uri)
+        http.before_request { |req| @callback.call req }
+        http
+      end
     end
 
     # Adds a callback to execute before each request (see `HTTP::Client#before_request`)
-    def before_request(&callback : HTTP::Request ->) : Nil
-      @callback = callback
+    def before_request(&@callback : HTTP::Request ->) : Nil
     end
 
     def call(rpc_name, request, response_type)
-      response = HTTP::Client.new(@uri) do |client|
-        if cb = @callback
-          client.before_request(&cb)
+      path = "#{@prefix}/#{T.service_name}/#{rpc_name}"
+      headers = HTTP::Headers{"Content-Type" => "application/protobuf"}
+      @pool.checkout do |http|
+        http.post(path, headers: headers, body: request.to_protobuf) do |response|
+          case response.content_type
+          when "application/json"
+            begin
+              raise Twirp::Error.from_json(response.body_io)
+            rescue err : JSON::SerializableError
+              raise Twirp::Error.new("Failed to parse JSON response: #{err}")
+            end
+          when "application/protobuf"
+            response_type.from_protobuf(response.body_io)
+          else
+            raise Twirp::Error.new("Unexpected response Content-Type: #{response.content_type}")
+          end
         end
-        client.post("#{@prefix}/#{T.service_name}/#{rpc_name}",
-          headers: HTTP::Headers{"Content-Type" => "application/protobuf"},
-          body: request.to_protobuf,
-        )
-      end
-
-      unless body = response.body?
-        raise Twirp::Error::Malformed.new("Failed to read response body")
-      end
-
-      case response.content_type
-      when "application/json"
-        begin
-          raise Twirp::Error.from_json(body)
-        rescue err : JSON::SerializableError
-          raise Twirp::Error.new("Failed to parse JSON response: #{err}")
-        end
-      when "application/protobuf"
-        response_type.from_protobuf(IO::Memory.new(body))
-      else
-        raise Twirp::Error.new("Unexpected response Content-Type: #{response.content_type}")
       end
     end
   end
